55d7b7bdc91717e7d0872bbc748bd4db97253abf
Jan Kawulok <jankawulok@madonair.pl>
2022-06-21 22:27:51 +0200

    initial commit

---
+++ CHANGELOG_de-DE.md
@@ -0,0 +1,3 @@
+# 1.0.0
+
+- Initiale Veröffentlichung
---
+++ src/Meilisearch.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Mdnr\Meilisearch;
+
+use Shopware\Core\Framework\Plugin;
+
+class Meilisearch extends Plugin
+{
+}
