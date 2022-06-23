6bb8f7531772fb9b152e00f8f425435f10bbf9cd
Jan Kawulok <jankawulok@madonair.pl>
2022-06-23 13:02:10 +0200

    fix: load vendor packages

--- src/Meilisearch.php
+++ src/Meilisearch.php
@@ -1,9 +1,21 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Mdnr\Meilisearch;
 
 use Shopware\Core\Framework\Plugin;
+use Composer\Autoload\ClassLoader;
 
 class Meilisearch extends Plugin
 {
+}
+
+// load vendor packages if plugin is not installed via composer
+if (\file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    $loader = require_once __DIR__ . '/../vendor/autoload.php';
+    if ($loader instanceof ClassLoader) {
+        $loader->unregister();
+        $loader->register(false);
+    }
 }
