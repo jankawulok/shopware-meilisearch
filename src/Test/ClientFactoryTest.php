2365e7d9798de485947aad37916ae015251d5a74
Jan Kawulok <jankawulok@madonair.pl>
2022-06-23 13:02:47 +0200

    feat: add clientFactory tests

---
+++ src/Test/ClientFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mdnr\Meilisearch\Test;
+
+use PHPUnit\Framework\TestCase;
+use Mdnr\Meilisearch\Framework\ClientFactory;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Core\System\SystemConfig\Util\ConfigReader;
+use Shopware\Core\System\SystemConfig\SystemConfigLoader;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Doctrine\DBAL\Connection;
+
+class ClientFactoryTest extends TestCase
+{
+    use IntegrationTestBehaviour;
+
+    public function testCreateClient(): void
+    {
+        $config = new SystemConfigService(
+            $this->getContainer()->get(Connection::class),
+            $this->getContainer()->get('system_config.repository'),
+            $this->getContainer()->get(ConfigReader::class),
+            $this->getContainer()->get(SystemConfigLoader::class),
+            $this->getContainer()->get('event_dispatcher')
+        );
+
+        $config->set('Meilisearch.config.urlEndpoint', 'http://127.0.0.1:7700');
+        $config->set('Meilisearch.config.masterKey', 'MASTER_KEY');
+
+        $clientFactory = new ClientFactory($config);
+        $client = $clientFactory->createClient();
+        static::assertSame('MeiliSearch\Client', get_class($client));
+        static::assertNotNull($client->getKeys());
+    }
+}
